40134504a6cc6a186f612f79e480f45caaebb7ed
David Pärsson <david@parsson.se>
2023-10-10 11:12:11 +0200

    Convert gruntfile to JavaScript

---
+++ gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+  require('jit-grunt')(grunt);
+
+  if (grunt.option('time')) {
+    require('time-grunt')(grunt);
+  }
+
+  grunt.initConfig({
+    jasmine_nodejs: {
+      options: {
+        specNameSuffix: ['Spec.js', 'Spec.coffee'],
+      },
+      test: {
+        specs: ['spec/**'],
+      },
+    },
+    watch: {
+      javaScript: {
+        files: ['src/**/*.js', 'spec/**/*.js'],
+        tasks: ['jshint', 'test'],
+        options: {
+          atBegin: true,
+        },
+      },
+      coffeeScript: {
+        files: ['spec/**/*.coffee'],
+        tasks: ['test'],
+      },
+      config: {
+        files: ['gruntfile.js'],
+        options: {
+          reload: true,
+        },
+      },
+      testResources: {
+        files: ['spec/**/*.xml'],
+        tasks: ['test'],
+      },
+    },
+    jshint: {
+      javaScript: ['src/**/*.js', 'spec/**/*.js'],
+    },
+  });
+
+  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('test', ['jasmine_nodejs:test']);
+  grunt.registerTask('tdd', ['watch']);
+};
