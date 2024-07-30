4320a17d1ca37e03b9964c18dbaba60d9078c0e1
Harel M <harel.mazor@gmail.com>
2024-07-30 11:29:52 +0000

    Prettier

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,27 +2,23 @@ import commonjs from '@rollup/plugin-commonjs';
 import noderesolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
-const config = [{
+const config = [
+  {
     input: ['src/index.ts'],
     output: {
-        file: 'dist/index.js',
-        format: 'cjs',
+      file: 'dist/index.js',
+      format: 'cjs',
     },
-    plugins: [
-        noderesolve(),
-        commonjs(),
-        typescript()
-    ]
-}, {
+    plugins: [noderesolve(), commonjs(), typescript()],
+  },
+  {
     input: ['src/index.ts'],
     output: {
-        file: 'dist/index.es.js',
-        format: 'es',
+      file: 'dist/index.es.js',
+      format: 'es',
     },
-    plugins: [
-        commonjs(),
-        typescript()
-    ]
-}];
+    plugins: [commonjs(), typescript()],
+  },
+];
 
 export default config;
