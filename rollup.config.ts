0e077e6dfde6d8455d637a45f991eb3c59ffbb2f
Harel M <harel.mazor@gmail.com>
2024-07-30 11:10:06 +0000

    Add es build type

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,20 +2,26 @@ import commonjs from '@rollup/plugin-commonjs';
 import noderesolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
+const plugins = [
+    noderesolve(),
+    commonjs(),
+    typescript()
+];
 
 const config = [{
     input: ['src/index.ts'],
     output: {
         file: 'dist/index.js',
-        name: 'junitReportBuilder',
         format: 'cjs',
     },
-    treeshake: true,
-    plugins: [
-        noderesolve(),
-        commonjs(),
-        typescript()
-    ]
+    plugins
+}, {
+    input: ['src/index.ts'],
+    output: {
+        file: 'dist/index.es.js',
+        format: 'es',
+    },
+    plugins
 }];
 
 export default config;
