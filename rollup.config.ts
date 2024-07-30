276cdfc03de327c11d8ac4cc9365d261bf7cc9c3
Harel M <harel.mazor@gmail.com>
2024-07-30 11:14:44 +0000

    Improve es build

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,26 +2,27 @@ import commonjs from '@rollup/plugin-commonjs';
 import noderesolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
-const plugins = [
-    noderesolve(),
-    commonjs(),
-    typescript()
-];
-
 const config = [{
     input: ['src/index.ts'],
     output: {
         file: 'dist/index.js',
         format: 'cjs',
     },
-    plugins
+    plugins: [
+        noderesolve(),
+        commonjs(),
+        typescript()
+    ]
 }, {
     input: ['src/index.ts'],
     output: {
         file: 'dist/index.es.js',
         format: 'es',
     },
-    plugins
+    plugins: [
+        commonjs(),
+        typescript()
+    ]
 }];
 
 export default config;
