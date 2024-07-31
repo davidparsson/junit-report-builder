2948c40202c63309b3d53f96a48e6dad0e8ec33e
David Pärsson <david@parsson.se>
2024-07-31 11:50:38 +0200

    Do not expose internal type

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,3 @@ export default new Factory().newBuilder();
 export type { JUnitReportBuilder as Builder } from './builder';
 export type { TestCase } from './test_case';
 export type { TestSuite } from './test_suite';
-export type { TestSuites } from './test_suites';
